6559842a82f65d3101f853a8eab22be5f680b234
JayMar <jayharronabejar@gmail.com>
2022-10-20 10:15:12 +0800

    Added Serilog implementation

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
 
 namespace WebApplication1
 {
@@ -7,11 +9,20 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
+            // use serilog file logger
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new RenderedCompactJsonFormatter())
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
